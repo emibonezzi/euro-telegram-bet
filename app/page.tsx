1a465392a49e8841efa64558556ceaca7ad91107
emibonezzi <emibonezzi@gmail.com>
2024-06-14 14:17:04 -0400

    Test on id

--- app/page.tsx
+++ app/page.tsx
@@ -33,9 +33,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        Welcome to Telegram Mini App, {user?.first_name && user.usernames}
-      </h1>
+      <h1>Welcome to Telegram Mini App, {user?.id}</h1>
     </main>
   );
 }
